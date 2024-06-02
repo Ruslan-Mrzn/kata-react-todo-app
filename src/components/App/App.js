434cb254dd6fd11439ba00575756e3be6979d0b0
Ruslan <russl.murzin@gmail.com>
2024-06-02 21:33:05 +0900

    feat: add interactivity to tasks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,11 +13,46 @@ export default class Api extends Component {
       tasks: [],
     }
     this.createNewTask = (text) => {
-      const newTask = { text: text, id: this.taskId++, condition: 'view' }
+      const newTask = { text: text, id: this.taskId++, condition: 'view', completed: false }
       console.log(newTask)
       this.setState(({ tasks }) => {
         return {
           tasks: [...tasks, newTask],
+        }
+      })
+    }
+    this.deleteTask = (taskId) => {
+      this.setState(({ tasks }) => {
+        const index = tasks.findIndex(({ id }) => id === taskId)
+        return {
+          tasks: [...tasks.slice(0, index), ...tasks.slice(index + 1)],
+        }
+      })
+    }
+    this.toggleCompleted = (taskId) => {
+      this.setState(({ tasks }) => {
+        const index = tasks.findIndex(({ id }) => id === taskId)
+        const markedTask = { ...tasks[index], completed: !tasks[index].completed }
+        return {
+          tasks: [...tasks.slice(0, index), markedTask, ...tasks.slice(index + 1)],
+        }
+      })
+    }
+    this.editTask = (taskId) => {
+      this.setState(({ tasks }) => {
+        const index = tasks.findIndex(({ id }) => id === taskId)
+        const editingTask = { ...tasks[index], condition: 'editing' }
+        return {
+          tasks: [...tasks.slice(0, index), editingTask, ...tasks.slice(index + 1)],
+        }
+      })
+    }
+    this.setEditedTask = (taskId, newText) => {
+      this.setState(({ tasks }) => {
+        const index = tasks.findIndex(({ id }) => id === taskId)
+        const editedTask = { ...tasks[index], text: newText, condition: 'view' }
+        return {
+          tasks: [...tasks.slice(0, index), editedTask, ...tasks.slice(index + 1)],
         }
       })
     }
@@ -33,8 +68,18 @@ export default class Api extends Component {
         </header>
         <section className="main">
           <TaskList>
-            {tasks.map(({ text, id, condition }) => (
-              <Task key={id} text={text} condition={condition} />
+            {tasks.map(({ text, id, condition, completed }) => (
+              <Task
+                toggleCompleted={this.toggleCompleted}
+                deleteTask={this.deleteTask}
+                id={id}
+                key={id}
+                text={text}
+                condition={condition}
+                completed={completed}
+                editTask={this.editTask}
+                setEditedTask={this.setEditedTask}
+              />
             ))}
           </TaskList>
           <Footer />
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react'
 import './Task.css'
 export default class Task extends Component {
+  constructor() {
+    super()
+    this.state = { newTaskText: '' }
+    this.typeNewTask = (e) => {
+      this.setState({ newTaskText: e.target.value })
+    }
+  }
   render() {
-    const { condition, text } = this.props
+    const { condition, text, deleteTask, id, toggleCompleted, completed, editTask, setEditedTask } = this.props
+    const { newTaskText } = this.state
     return (
-      <li className={condition}>
+      <li className={completed ? 'completed' : condition}>
         <div className="view">
-          <input className="toggle" type="checkbox" />
+          <input onChange={() => toggleCompleted(id)} className="toggle" type="checkbox" />
           <label>
             <span className="description">{text}</span>
             <span className="created">created 5 minutes ago</span>
           </label>
-          <button className="icon icon-edit"></button>
-          <button className="icon icon-destroy"></button>
+          <button onClick={() => editTask(id)} className="icon icon-edit"></button>
+          <button onClick={() => deleteTask(id)} className="icon icon-destroy"></button>
         </div>
-        <input type="text" className="edit" value={text} />
+        <input
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              setEditedTask(id, newTaskText)
+              this.setState({ newTaskText: e.target.value })
+            }
+          }}
+          onChange={this.typeNewTask}
+          type="text"
+          className="edit"
+          defaultValue={text}
+        />
       </li>
     )
   }
